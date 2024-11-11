1d499bc2480b785b26bd27442573df16f33b1094
borso271 <ethereum271@gmail.com>
2024-11-11 14:34:29 +0000

    Add files via upload

---
+++ docs/game/colors.js
@@ -0,0 +1,7 @@
+
+export const immovablecolor = "black";
+export const immovableborder = "#00ffff";
+export const wallcolor = "white";
+export const wallborder = "red";
+export const pavementcolor = "black";
+export const pavementinner = 'white';
