c46d8c9ca03f1de801451eba70bb9b588ef901e3
borso271 <ethereum271@gmail.com>
2024-11-11 14:33:58 +0000

    Delete docs directory

--- docs/game/colors.js
+++
@@ -1,7 +0,0 @@
-
-export const immovablecolor = "black";
-export const immovableborder = "#00ffff";
-export const wallcolor = "white";
-export const wallborder = "red";
-export const pavementcolor = "black";
-export const pavementinner = 'white';
